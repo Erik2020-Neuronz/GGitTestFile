8c13a61bacacd55266d30768c5b1530bfc7a5649
Erik2020-Neuronz <erik2020@vt.edu>
2021-04-05 12:23:18 -0400

    Testing Commit
    should add JustATest.m

---
+++ JustATest.m
@@ -0,0 +1,23 @@
+
+
+
+clear all;
+g = 77:1:81;
+g = g';
+
+gg = 1:3:24;
+gg = gg';
+
+h = 45:5:55;
+h = h';
+
+thisKid{1,1} = g;
+thisKid{2,1} = gg;
+thisKid{3,1} = h;
+
+
+for i = 1:3
+   
+    myTest = thisKid{i,1};
+    
+end
